dbbd91880438651d1cc6a8dbaa5c8a40ff3cca16
Paul Zhang <paulz@ucla.edu>
2022-11-01 13:27:59 -0700

    Use custom proxy for accessing backend
    Better than using the `proxy` field in `package.json`, since that
    requires setting the environment variable DANGEROUSLY_DISABLE_HOST_CHECK
    to true.
    starting

---
+++ src/setupProxy.js
@@ -0,0 +1,12 @@
+// Proxies /api requests to the backend
+const { createProxyMiddleware } = require("http-proxy-middleware")
+
+module.exports = function (app) {
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: "http://localhost:3001",
+      changeOrigin: true,
+    })
+  )
+}
