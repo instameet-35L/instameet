8856de1e56039f9e4d069633dda03a378bf8a6ba
Arathi Nair <arathinair@g.ucla.edu>
2022-11-01 23:50:39 -0700

    Minor changes to calendar formatting with date/value array

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import "./App.css"
-//import Calendar from "./calendar.js"
 import React, { useState } from 'react';
 import Calendar from "react-calendar"
 import 'react-calendar/dist/Calendar.css';
+
+//this file implements the calendar component
+//value is the date(s) selected by the user when they click
+
+/* 
+  BUGS TO FIX:
+  -how to unselect range and go back to a singular day
+*/
 
 function App() {
   const [value, setValue] = useState(new Date());
@@ -11,23 +18,24 @@ function App() {
       <div className='text-center'>
         <h1 align='center'>Instameet! 🤖</h1>
       </div>
-      <h1 className='text-center'>React Calendar with Range</h1>
+      {/* add text box to name the schedule here */}
       <div className='calendar'>
         <Calendar 
-              onChange={setValue} 
-              value={value} 
-              selectRange={true}/>
+              onChange={setValue} //when a date is clicked
+              value={value}   //date = value
+              selectRange={true}  //can select a range of dates
+              />  
       </div>
       <div>
-        {value.length > 0 ? (
+        {value.length > 1 ? ( //if more than one date is selected
           <p className='text-center'>
             <span className='bold'>Start:</span>{' '}{value[0].toDateString()}
             &nbsp;&nbsp;|&nbsp; &nbsp;
             <span className='bold'>End:</span>{' '}{value[1].toDateString()}
           </p>
-        ) : (
-          <p className='text-center'>
-            <span className='bold'>Selected Date(s):</span>{' '}{value.toDateString()}
+        ) : ( //if 1 date is selected
+          <p className='text-center'> 
+            <span className='bold'>Selected Date:</span>{' '}{value.toDateString()}
           </p>
         )}
       </div>
