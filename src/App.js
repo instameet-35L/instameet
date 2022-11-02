36485612f809eed69925e217782d9e8a0dbca684
Arathi Nair <arathinair@g.ucla.edu>
2022-11-01 23:10:50 -0700

    Moved everything back to app.js from calendar.js and added preliminary date selection and ranges

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import "./App.css"
+//import Calendar from "./calendar.js"
 import React, { useState } from 'react';
 import Calendar from "react-calendar"
+import 'react-calendar/dist/Calendar.css';
 
 function App() {
+  const [value, setValue] = useState(new Date());
   return (
-    <div id="main">
-      <h1>Instameet! 🤖</h1>
-      <Calendar />
+    <div className="main">
+      <div className='text-center'>
+        <h1 align='center'>Instameet! 🤖</h1>
+      </div>
+      <h1 className='text-center'>React Calendar with Range</h1>
+      <div className='calendar'>
+        <Calendar 
+              onChange={setValue} 
+              value={value} 
+              selectRange={true}/>
+      </div>
+      <div>
+        {value.length > 0 ? (
+          <p className='text-center'>
+            <span className='bold'>Start:</span>{' '}{value[0].toDateString()}
+            &nbsp;&nbsp;|&nbsp; &nbsp;
+            <span className='bold'>End:</span>{' '}{value[1].toDateString()}
+          </p>
+        ) : (
+          <p className='text-center'>
+            <span className='bold'>Selected Date(s):</span>{' '}{value.toDateString()}
+          </p>
+        )}
+      </div>
+
     </div>
   )
 }
--- src/calendar.js
+++ src/calendar.js
@@ -1,19 +1,19 @@
-import React, { useState } from 'react';
-import Calendar from "react-calendar"
-import 'react-calendar/dist/Calendar.css';
+// import React, { useState } from 'react';
+// import Calendar from "react-calendar"
+// import 'react-calendar/dist/Calendar.css';
 
-export default function MyCal() {
+// export default function MyCal() {
 
-    const [value, setValue] = useState(new Date());
+//     const [value, setValue] = useState(new Date());
     
-    function onChange(nextValue) {
-        console.log("HI");
-        setValue(nextValue);
-      }
+//     // function onChange(nextValue) {
+//     //     console.log("HI");
+//     //     setValue(nextValue);
+//     //   }
     
-    return (
-      <div>
-        <Calendar onChange={i => onChange(i)} value={value} />
-      </div>
-    );
-  }
+//     return (
+//       <div>
+//         <Calendar onChange={setValue} value={value} />
+//       </div>
+//     );
+//   }
