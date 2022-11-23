21c5951a338ea8f9afd4601d6964e9b17e9dd754
Paul Zhang <paulz@ucla.edu>
2022-11-22 23:12:58 -0800

    Add styling to CreateMeeting and TitleForm

--- src/components/CreateMeeting.js
+++ src/components/CreateMeeting.js
@@ -1,9 +1,9 @@
 import { useState } from "react"
-import { Navigate, redirect } from "react-router-dom"
-import "../css/CreateMeeting.css"
+import { Navigate } from "react-router-dom"
 
 export default function CreateMeeting({ title, timeframe, creator }) {
-  const [meetingId, setMeetingId] = useState("")
+  const [meetingId, setMeetingId] = useState(null)
+  const [isValid, setIsValid] = useState(true)
 
   async function requestNewMeeting() {
     const newMeetingData = {
@@ -24,20 +24,31 @@ export default function CreateMeeting({ title, timeframe, creator }) {
     const data = await response.json()
 
     if (response.status !== 200) {
-      console.log("uh oh")
+      setIsValid(false)
     }
 
     setMeetingId(data._id)
   }
 
   return (
-    <>
-      {meetingId !== "" && meetingId !== undefined && (
+    <div class="flex flex-col items-center space-y-5">
+      {meetingId !== null && meetingId !== undefined && (
         <Navigate to={`/${meetingId}`} />
       )}
-      <button className="buttonMeeting" onClick={requestNewMeeting}>
+      <button
+        class="text-base font-medium rounded-lg p-3 bg-cyan-600 text-white w-max"
+        onClick={requestNewMeeting}
+      >
         Create Meeting
       </button>
-    </>
+      {!isValid && (
+        <div
+          class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
+          role="alert"
+        >
+          <p class="font-bold">u done messed up ur input</p>
+        </div>
+      )}
+    </div>
   )
 }
--- src/components/TitleForm.js
+++ src/components/TitleForm.js
@@ -1,10 +1,26 @@
 export default function TitleForm({ setTitle }) {
   return (
-    <form>
-      <label for="title">Title:</label>
+    <form class="mb-3 xl:w-96">
+      <label class="form-label inline-block mb-2 text-gray-700" for="title">
+        Title:
+      </label>
       <input
         type="text"
-        name="title"
+        class="form-control
+        block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
         id="title"
         onChange={(event) => {
           setTitle(event.target.value)
