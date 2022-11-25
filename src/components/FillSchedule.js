573eeeebd5280c69fe60d2a7e9c7e40acbb758e6
MissyB <missy789@g.ucla.edu>
2022-11-25 14:17:25 -0800

    Create a column to the left of the grid displaying the time representing each grid row

--- src/components/FillSchedule.js
+++ src/components/FillSchedule.js
@@ -26,7 +26,15 @@ function DateEntry(props) {
   return (
     <div className="dateEntry">
       date
-      {props.value}
+      {props.value1}
+    </div>
+  )
+}
+
+function TimeEntry(props) {
+  return (
+    <div className="dateEntry">
+      {props.value2}
     </div>
   )
 }
@@ -36,7 +44,12 @@ class Board extends React.Component {
     super(props)
     this.state = {
       entries: Array(100).fill(null), // should this be array(100) or should there not be a specific sizE??
-      dateEntries: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
+      dateEntries: ["", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],  // put each date here
+      timeEntries: ["6:00 AM", "7:00 AM", "8:00 AM",
+        "9:00 AM", "10:00 AM", "11:00 AM", "12:00 PM", 
+        "1:00 PM", "2:00 PM", "3:00 PM", "4:00 PM", 
+        "5:00 PM", "6:00 PM", "7:00 PM", "8:00 PM",
+        "9:00 PM", "10:00 PM"]
     }
   }
 
@@ -51,6 +64,14 @@ class Board extends React.Component {
 
   // loopDates(i){
 
+  // }
+
+  // updateDateEntries() {
+  //   const tempEntries = this.state.dateEntries.slice()
+  //   tempEntries = ["theDate", "the next date", 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14]
+  //   this.setState({
+  //     dateEntries: tempEntries
+  //   })
   // }
 
   renderEntry(i) {
@@ -68,7 +89,15 @@ class Board extends React.Component {
   renderDateEntry(i) {
     return (
       <DateEntry
-        value={this.state.dateEntries[i]}
+        value1={this.state.dateEntries[i]}
+      />
+    )
+  }
+
+  renderTimeEntry(i) {
+    return (
+      <TimeEntry
+        value2={this.state.timeEntries[i]}
       />
     )
   }
@@ -79,12 +108,19 @@ class Board extends React.Component {
     let dateRows = []
     const numCols = 6 // would just need arathi's num from the calendar
     let dateChildren = []
-    for (let r = 0; r < 24; r++) {
+    let timeI = 0
+    for (let r = 0; r < 17; r++) {
       let children = []
       let scale = r * numCols
-
       for (let c = 0 + scale; c < numCols + scale; c++) {
-        children.push(this.renderEntry(c))
+        if ((c + scale) % scale === 0 || c === 0)
+        {
+          children.push(this.renderTimeEntry(timeI))
+          timeI++
+        }
+        else {
+          children.push(this.renderEntry(c))
+        }
       }
 
       rows.push(
