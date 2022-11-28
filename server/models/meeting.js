6d7f9e86da168636a0135527e06324462fa3b3c6
Paul Zhang <paulz@ucla.edu>
2022-11-27 19:16:30 -0800

    Modify schema to include users' availability

--- server/models/meeting.js
+++ server/models/meeting.js
@@ -17,15 +17,29 @@ const meetingSchema = new mongoose.Schema({
     },
   },
 
-  times: {
-    type: Map,
-    of: [Number], // IDs of users available during this time
-  },
-
   users: [
     {
-      mongoId: Number,
-      name: String,
+      name: {
+        type: String,
+        unique: true,
+        required: true,
+      },
+
+      // Stores a user's available times as grid cell indices.
+      //
+      // 0 corresponds to the earliest time (currently hardcoded to 6:00 am) of
+      // the first day (stored in `timeframe.start` of this schema). 1
+      // corresponds to the earliest time of the second day, and so on.
+      //
+      // For example, given that the earliest time is 6:00 am, the start date is
+      // Jan. 1, and the end date is Jan. 3:
+      //
+      // `available = [0, 1, 3, 5]` means the user is available on 6 am of Jan.
+      // 1, 6 am of Jan. 2, 7 am of Jan. 1, and 7 am of Jan. 3, respectively.
+      available: {
+        type: [Number],
+        required: true,
+      },
     },
   ],
 })
