e094cc4f5e1e8f5c30e0553c77595016ad22c44b
Claire Chen <clairechen@s-169-232-126-144.resnet.ucla.edu>
2022-11-28 21:12:43 -0800

    implemented X button to close the notifications also set threshold for notif to show up

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,38 +1,52 @@
-import React from "react";
+import React, { useState } from "react"
 // import { useEffect, useState } from "react"
-import '../css/Popup.css'
+import "../css/Popup.css"
 
 /**
- * 
+ *
  * @param {*} props: totalAmount, howManyHaveSignedUp
- * @returns 
+ * @returns
  */
 
-const Popup = (props) => {
+const CloseButton = (props) => (
+  <div className="close">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={24}
+      height={24}
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="feather feather-x"
+      {...props}
+    >
+      <path d="M18 6 6 18M6 6l12 12" />
+    </svg>
+  </div>
+)
 
-    return (
-        <div>
-            <div className="wrapper">
-                <div className="notification_wrapper">
-                    Notification:      
-                    
-                </div>
-                <div className="contentfraction">
-                    {props.registered} people have filled their availability
-          
-                </div>
-                <div className ="contenttime">
-                    {props.time} is the best time
-                </div>
-                <div className="button bg-blue-400">
-                   Done
-                </div>
+const Popup = (props) => {
+  const [show, setShow] = useState(props.registered >= 5 ? true : false)
+  return (
+    <div>
+      {show && (
+        <div className="wrapper">
+          <div className="notification_wrapper">
+            Notification:
+            <div onClick={() => {setShow(!show)}}>
+              <CloseButton />
             </div>
+          </div>
+          <div className="contentfraction">
+            {props.registered} people have filled their availability
+          </div>
+          <div className="contenttime">{props.time} is the best time</div>
         </div>
-    )
+      )}
+    </div>
+  )
 }
 
-export default Popup;
-
-
-
+export default Popup
